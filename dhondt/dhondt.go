c382d887d7f90fd5ee7904267f4e49cf91b5fae9
ferno <fer.pereira9014@gmail.com>
2021-11-14 01:08:15 -0300

    New iteration calculating two parties

--- dhondt/dhondt.go
+++ dhondt/dhondt.go
@@ -3,8 +3,9 @@ package dhondt
 import "github.com/fer90/dhondt-system/party"
 
 func Dhondt(parties []party.Party, availableSeats int) map[string]int {
-	result := map[string]int {
-		parties[0].Name: availableSeats,
+	result := make(map[string]int)
+	for _,party := range parties {
+		result[party.Name] = availableSeats / cap(parties)
 	}
 	return result
 }
