482683ba52a6158211129893b5da30312efe2982
ferno <fer.pereira9014@gmail.com>
2021-11-14 23:15:39 -0300

    Refactoring with some modularization

--- dhondt/dhondt.go
+++ dhondt/dhondt.go
@@ -3,6 +3,7 @@ package dhondt
 import (
 	"errors"
 	"strings"
+	"strconv"
 	"github.com/fer90/dhondt-system/party"
 )
 
@@ -10,25 +11,40 @@ const MINIMAL_AVAILABLE_SEATS = 1
 
 func Dhondt(parties []party.Party, availableSeats int) (map[string]int, error) {
 	if (availableSeats < MINIMAL_AVAILABLE_SEATS) {
-		return nil, errors.New("Available seats should be greater than zero")
+		return nil, errors.New("Available seats should be at least " + strconv.Itoa(MINIMAL_AVAILABLE_SEATS))
 	}
+	adjudicatedSeatByParty, votesRemaining := initializeDhondtCalculation(parties)
+	for availableSeats > 0 {
+		winnerPartyForNextSeat := calculatePartyForNextSeat(votesRemaining, parties)
+		adjudicatedSeatByParty[winnerPartyForNextSeat.Name]++
+		availableSeats--
+		votesRemaining[winnerPartyForNextSeat.Name] = winnerPartyForNextSeat.ValidVotes / (adjudicatedSeatByParty[winnerPartyForNextSeat.Name] + 1)
+	}
+	return adjudicatedSeatByParty, nil
+}
+
+func initializeDhondtCalculation(parties []party.Party) (map[string]int, map[string]int) {
 	result := make(map[string]int)
 	votesRemaining := make(map[string]int)
 	for _,party := range parties {
 		result[party.Name] = 0
 		votesRemaining[party.Name] = party.ValidVotes / (result[party.Name] + 1)
 	}
-	adjudicatedSeats := 0
-	for adjudicatedSeats != availableSeats {
-		winnerPartyForNextSeat := calculatePartyForNextSeat(votesRemaining, parties)
-		result[winnerPartyForNextSeat.Name]++
-		adjudicatedSeats++
-		votesRemaining[winnerPartyForNextSeat.Name] = winnerPartyForNextSeat.ValidVotes / (result[winnerPartyForNextSeat.Name] + 1)
-	}
-	return result, nil
+	return result, votesRemaining
 }
 
 func calculatePartyForNextSeat(votesRemaining map[string]int, parties []party.Party) party.Party {
+	partyNameWithMostVotes := getPartyNameWithMostVotes(votesRemaining)
+	var winnerParty party.Party
+	for _,party := range parties {
+		if strings.Compare(partyNameWithMostVotes, party.Name) == 0 {
+			winnerParty = party
+		}
+	}
+	return winnerParty
+}
+
+func getPartyNameWithMostVotes(votesRemaining map[string]int) string {
 	maxVotes := 0
 	partyNameWithMostVotes := ""
 	for partyName,votes := range votesRemaining {
@@ -37,11 +53,5 @@ func calculatePartyForNextSeat(votesRemaining map[string]int, parties []party.Pa
 			partyNameWithMostVotes = partyName
 		}
 	}
-	var winnerParty party.Party
-	for _,party := range parties {
-		if strings.Compare(partyNameWithMostVotes, party.Name) == 0 {
-			winnerParty = party
-		}
-	}
-	return winnerParty
+	return partyNameWithMostVotes
 }
